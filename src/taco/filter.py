9834fc8c36aaa7b3eb725b285485e3b057db7216
Bernd Doser <bernd.doser@h-its.org>
2022-06-13 17:00:52 +0200

    add options to filter

--- src/taco/filter.py
+++ src/taco/filter.py
@@ -4,7 +4,7 @@ from rpy2.robjects import pandas2ri
 
 from rpy2.robjects.conversion import localconverter
 
-def filter(ts):
+def filter(ts, width, remove_gaps):
     """
     Filtering the lightcurves with a triangular smooth
     I use two rectangular smooths with half the provided width.
@@ -17,6 +17,8 @@ def filter(ts):
             Columns:
                 Name: time, dtype: datetime64[days]
                 Name: flux, dtype: int64
+        width(int):Width of the triangular filter
+        remove_gaps(int):Remove gaps greater than this value (in days). If set to -1, do not remove gaps
     """
 
     base = importr('base')
