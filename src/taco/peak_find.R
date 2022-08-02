9c49c6a80635e4a0dd06a1f8777d1fc69d8f5cb9
Bernd Doser <bernd.doser@h-its.org>
2022-08-02 16:57:48 +0200

    add parameters to peak_find

--- src/taco/peak_find.R
+++ src/taco/peak_find.R
@@ -11,7 +11,8 @@ source("src/peakFind_lib.R", chdir = TRUE)
 # 22/12/19 adding this to enable estimation of d02 from numax
 source("src/l02_modes_id.R", chdir = TRUE)
 
-peak_find_r <- function(pds, ofac_pds, removel02) {
+peak_find_r <- function(pds, ofac_pds, peaks, mixedpeaks, snr, prob,
+                        maxlwd, removel02, minAIC, navg) {
 
     deltanu <- diff(pds$frequency[1:2])
     ofac_deltanu <- diff(ofac_pds$frequency[1:2])
@@ -25,62 +26,60 @@ peak_find_r <- function(pds, ofac_pds, removel02) {
     #     # Finding mixed modes!
 
     #     print("Removing l=0,2,3 first")
-    #     d.peaks <- try(read_csv(argv$peaks, col_types = cols()) %>%
-    #                         filter(frequency > d.summary$numax - 3*d.summary$sigmaEnv,
-    #                             frequency < d.summary$numax + 3*d.summary$sigmaEnv))
-    #     if("try-error" %in% class(d.peaks)){
-    #         d.pds_l02_removed <- d.pds
-
+    #     peaks %>% filter(frequency > summary$numax - 3*summary$sigmaEnv,
+    #                      frequency < summary$numax + 3*summary$sigmaEnv))
+    #     if("try-error" %in% class(peaks)){
+    #         pds_l02_removed <- pds
     #     } else{
 
-    #         if(nrow(d.peaks) != 0){
-    #             d.l02_peaks <-
-    #                 d.peaks %>%
+    #         if(nrow(peaks) != 0){
+    #             l02_peaks <-
+    #                 peaks %>%
     #                 filter(l == 0 | l == 2 | l == 3)
-    #             d.pds_l02_removed <-
-    #                 d.pds %>%
-    #                 mutate(power = power / fit_model(pds = ., peaks = d.l02_peaks))
+    #             pds_l02_removed <-
+    #                 pds %>%
+    #                 mutate(power = power / fit_model(pds = ., peaks = l02_peaks))
     #         } else{
-    #             d.pds_l02_removed <- d.new_pds
+    #             pds_l02_removed <- new_pds
     #         }
     #     }
 
     #     # If max linewidth argument not set
     #     #stop()
-    #     if (is.na(argv$maxlwd)){
+    #     if (is.na(maxlwd)){
     #         # Since this is for finding mixed modes we add in constraint that linewidth must be less than Gamma0
-    #         if(is.null(d.summary$gamma0)) {
+    #         if(is.null(summary$gamma0)) {
     #             maxlwd <- deltanu/2 + 0.1*deltanu/2
     #             print(paste("Gamma0 from summary file is NA therefore setting to slightly larger than bin width: ", maxlwd, "uHz"))
     #             #stop("Gamma0 from summary file is NA please supply a maximum linewidth")
     #         } else {
-    #             maxlwd <- 1.5*d.summary$gamma0
+    #             maxlwd <- 1.5*summary$gamma0
     #             print(paste("Maximum peak linewidth (HWHM) not set, therefore set to Gamma0: ", maxlwd, "uHz"))
     #         }
     #     } else{
-    #         maxlwd <- 1.5*as.numeric(argv$maxlwd)
+    #         maxlwd <- 1.5*as.numeric(maxlwd)
     #         print(paste("Maximum peak linewidth (HWHM) set, using value ", maxlwd, "uHz"))
     #     }
     #     # 23/12/19 Because we are fitting in terms of HWHM the minimum linewidth should be bw/2 not bw!
 
-    #     d.new_peaks <- peak_find(d.pds_l02_removed, min.snr = argv$snr, p = argv$prob,
+    #     new_peaks <- peak_find(pds_l02_removed, min.snr = snr, p = prob,
     #                         linewidth.range = c(deltanu/2, maxlwd),
-    #                         find.resolved.only=FALSE, naverages=argv$navg)
+    #                         find.resolved.only=FALSE, naverages=navg)
     #     ## Choose only the significant ones
 
-    #     d.new_peaks <-
-    #         d.new_peaks %>%
+    #     new_peaks <-
+    #         new_peaks %>%
     #         arrange(frequency) %>%
-    #         #mutate(EFP = nrow(d.pds_l02_removed) * EFPp) %>%
-    #         filter(AIC > argv$minAIC)# %>%
+    #         #mutate(EFP = nrow(pds_l02_removed) * EFPp) %>%
+    #         filter(AIC > minAIC)# %>%
     #         #filter(EFP < 1) # Also added this line to see if this helps trim away bad peaks!
-    #     write_csv(d.new_peaks, file = argv$mixedpeaks)#argv$output)
+    #     write_csv(new_peaks, file = mixedpeaks)#output)
 
     # } else {
     #     #  Only find resolved peaks
     #     # If value not given then set it to be equal to 0.1 * dnu or just less than ~d02
-    #     if (is.na(argv$maxlwd)){
-    #         deltanu_est <- DeltaNu_from_numax(d.summary$numax)
+    #     if (is.na(maxlwd)){
+    #         deltanu_est <- DeltaNu_from_numax(summary$numax)
     #         # Set to be < d02 from scaling relation (~0.125 dnu)
     #         # Divide by 2 because HWHM defined here and want FWHM to be less than ~d02
     #         maxlwd <-  0.125 * deltanu_est / 2
@@ -90,18 +89,16 @@ peak_find_r <- function(pds, ofac_pds, removel02) {
     #         }
     #         print(paste("Maximum peak linewidth (HWHM) not set, therefore taking estimated value ", maxlwd, "uHz"))
     #     } else{
-    #         maxlwd <- as.numeric(argv$maxlwd)
+    #         maxlwd <- as.numeric(maxlwd)
     #         print(paste("Maximum peak linewidth (HWHM) set, using value ", maxlwd, "uHz"))
     #     }
 
-    #     print(deltanu)
-
-    #     d.peaks <- peak_find(d.pds, min.snr = argv$snr, p = argv$prob,
+    #     peaks <- peak_find(pds, min.snr = snr, p = prob,
     #                         linewidth.range = c(deltanu/2, maxlwd),
-    #                         find.resolved.only=TRUE, naverages=argv$navg)
+    #                         find.resolved.only=TRUE, naverages=navg)
 
-    #     if (is.null(d.peaks)){
-    #         d.peaks <- tibble(frequency=double(),
+    #     if (is.null(peaks)){
+    #         peaks <- tibble(frequency=double(),
     #                             linewidth=double(),
     #                             height=double(),
     #                             snr=double(),
@@ -109,19 +106,19 @@ peak_find_r <- function(pds, ofac_pds, removel02) {
     #                             amplitude=double(),
     #                             EFPp=double(),
     #                             EFP=double())
-    #         write_csv(d.peaks, file=argv$output)
+    #         write_csv(peaks, file=output)
     #         print("No resolved peaks found!")
     #     } else{
 
     #         ## Choose only the significant ones
-    #         d.peaks <-
-    #             d.peaks %>%
+    #         peaks <-
+    #             peaks %>%
     #             arrange(frequency) %>%
-    #             #mutate(EFP = nrow(d.pds) * EFPp) %>%
-    #             filter(AIC > argv$minAIC)# %>%
+    #             #mutate(EFP = nrow(pds) * EFPp) %>%
+    #             filter(AIC > minAIC)# %>%
     #             #filter(EFP < 0.5) # Also added this line to see if this helps trim away bad peaks!
 
-    #         write_csv(d.peaks, file = argv$output)
+    #         write_csv(peaks, file = output)
     #     }
     # }
 }
--- src/taco/peak_find.py
+++ src/taco/peak_find.py
@@ -7,7 +7,8 @@ from rpy2.robjects.conversion import localconverter
 from rpy2.robjects.packages import STAP
 
 
-def peak_find(pds, oversampled_pds, removel02 = False):
+def peak_find(pds, oversampled_pds, peaks, mixedpeaks, snr = 1.2, prob = 0.0001,
+              maxlwd = NA, removel02 = False, minAIC = 2, navg = 1):
     """
     Find the relevant solar-like oscillations in a background-removed PDS
     We use a tree-map of the local maxima found by a (mexican-hat) wavelet
@@ -23,7 +24,23 @@ def peak_find(pds, oversampled_pds, removel02 = False):
             Columns:
                 Name: frequency, dtype: float[micro-Hertz]
                 Name: power, dtype: float
+        peaks(pandas.DataFrame): Identified peaks. It must contain the l=0,2 modes already identified
+            Columns:
+                Name: frequency
+                Name: linewidth
+                Name: height
+                Name: snr
+                Name: AIC
+                Name: amplitude
+        mixedpeaks(pandas.DataFrame): Mixed mode peaks from peak finding
+            Columns:
+                Name: frequency
+        snr(float): Minimum signal-to-noise ratio (on CWT space) for resolved peaks
+        prob(float): Minimum (frequentist) probability threshold for unresolved peaks
+        maxlwd(float): Maximum search linewidth for resolved peaks in the CWT search
         removel02(bool): Whether or not the l02 peaks should be divided out before running the CWT search
+        minAIC(int): Minimum AIC value for a peak to have in order to be considered significant
+        navg(int): Number of power spectra averaged to create current power spectrum
     """
 
     with open(Path(Path(__file__).parent, 'peak_find.R'), 'r') as f:
