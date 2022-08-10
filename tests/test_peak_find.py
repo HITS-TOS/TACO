d5ce21d7ed1104fb42798e0f8cef5725d646594b
Bernd Doser <bernd.doser@h-its.org>
2022-08-10 14:23:08 +0200

    add AIC for test_peak_find_with_peaks

--- tests/test_peak_find.py
+++ tests/test_peak_find.py
@@ -8,11 +8,10 @@ def test_peak_find():
     peaks = peak_find(pds, oversampled_pds)
     assert "frequency" in peaks
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_peak_find_with_peaks():
     pds = pd.DataFrame({"frequency": [1, 2], "power": [1, 0]})
     oversampled_pds = pd.DataFrame({"frequency": [0.5, 1, 1.5, 2], "power": [0, 1, 0, 0]})
-    peaks = pd.DataFrame({"frequency": [1]})
+    peaks = pd.DataFrame({"frequency": [1], "AIC": [1]})
     mixed_peaks = pd.DataFrame({"frequency": [1]})
     peaks = peak_find(pds, oversampled_pds, peaks, mixed_peaks)
     assert "frequency" in peaks
