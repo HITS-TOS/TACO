42fa3af7377f00382551b93386dfa65027163986
Bernd Doser <bernd.doser@h-its.org>
2022-08-31 15:15:47 +0200

    fix numax0_flag

--- tests/test_numax_estimate.py
+++ tests/test_numax_estimate.py
@@ -9,7 +9,7 @@ def test_numax_001296068():
     pds = pd.read_csv("tests/data/test_numax_4_pds.csv")
     data = pd.read_csv("tests/data/test_numax_4_summary.csv")
     data = numax_estimate(pds, data)
-    assert data["numax0_flag"][0] is False
+    assert not data["numax0_flag"][0]
     assert data["numax_var"][0] == pytest.approx(43.961062750337454, 1e-6)
     assert data["numax_CWTMexHat"][0] == pytest.approx(61.42446564103499, 1e-6)
     assert data["numax_Morlet"][0] == pytest.approx(59.333183743645144, 1e-6)
@@ -22,6 +22,6 @@ def test_numax_small():
     pds = pd.read_csv("tests/data/test_numax_3_pds.csv")
     data = pd.read_csv("tests/data/test_numax_3_summary.csv")
     data = numax_estimate(pds, data)
-    assert data["numax0_flag"][0] is False
+    assert not data["numax0_flag"][0]
     assert data["numax_var"][0] == pytest.approx(43.961062750337454, 1e-6)
     assert data["numax_CWTMexHat"][0] == pytest.approx(61.42446564103499, 1e-6)
