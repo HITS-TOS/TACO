a3bc2afc279c77e7f4fc518669a0faaabc4e4dd5
saskiahekker <saskia.hekker@h-its.org>
2024-12-03 13:42:37 +0100

    changed the results to result in app and reduced the requirement on linewidth for l=3 modes to deltanu

--- src/taco/peak_bag_mode_id02.R
+++ src/taco/peak_bag_mode_id02.R
@@ -322,7 +322,7 @@ peak_bag_mode_id02_r <- function(pds, peaks, data, contour) {
 
             # Make sure there is a nearest l=0 before doing this
             if (is.na(tmp_l3$l) && !is.na(tmp_l3$linewidth)) {
-                if ((nrow(closest_l0_amp) > 0) && (nrow(closest_l0_width) > 0) && tmp_l3$linewidth > 2 * deltanu) {
+                if ((nrow(closest_l0_amp) > 0) && (nrow(closest_l0_width) > 0) && tmp_l3$linewidth > deltanu) {
                         peaks$l[peaks$frequency == tmp_l3$frequency] <- 3
                         # Subtract one from nearest l=0 radial order to ensure correct
                         peaks$n[peaks$frequency == tmp_l3$frequency] <- tmp_l3$n - 1
